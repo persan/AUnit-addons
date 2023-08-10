30598e9aa71468ddbf90fd3c351a45c8f5bf9c84
Per Sandberg <per.sandberg@saabgroup.com>
2023-08-10 10:45:26 +0200

    Clean up

---
+++ tests/src/tc-main.ads
@@ -0,0 +1,2 @@
+with AUnit.Test_Cases.Simple_Main_Generic;
+procedure Tc.Main is new AUnit.Test_Cases.Simple_Main_Generic (Test_Case);
--- tests/src/tc-register_tests.adb
+++ tests/src/tc-register_tests.adb
@@ -4,7 +4,7 @@ overriding procedure Register_Tests (Test : in out Test_Case) is
    use AUnit.Test_Cases.Registration;
 begin
    Register_Routine (Test, Test_Routine'Unrestricted_Access, "Test_Routine");
-   Register_Routine (Test, Test_Routine2'Unrestricted_Access, "Test_Routine2");
+   Register_Routine (Test, Test_Routine33'Unrestricted_Access, "Test_Routine33");
    Register_Routine (Test, Test_Routine4'Unrestricted_Access, "Test_Routine4");
 end Register_Tests;
 --  end read only
